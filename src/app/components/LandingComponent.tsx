174d03a18fd29d2def9443dc93a84f573ff15d19
sonprogrammer <ods04138@gmail.com>
2026-09-11 23:48:08 +0900

    feat: login

--- src/app/components/LandingComponent.tsx
+++ src/app/components/LandingComponent.tsx
@@ -1,39 +1,8 @@
-'use client'
 
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/navigation'
 import LoginSection from './LoginSection'
-import { axiosInstance } from '@/lib/axios'
-import Loading from '@/app/components/Loading'
-import { useUserStore } from '@/store/userStore'
 
-export default function LandingComponent({ hasRefreshToken }: { hasRefreshToken: boolean }) {
-  const router = useRouter()
-  const [loading, setLoading] = useState(false)
-  const setUserAccessToken = useUserStore(state => state.setUserAccessToken)
+export default function LandingComponent() {
 
-
-
-
-
-  useEffect(() => {
-    if (!hasRefreshToken) return
-    const autoLogin = async () => {
-      try {
-        const res = await axiosInstance.post('/autoLogin')
-        setUserAccessToken(res.data.accessToken)
-        router.replace('/home')
-      } catch (err) {
-        console.log('faile', err)
-        setLoading(false)
-      }
-    }
-    autoLogin()
-  }, [router, hasRefreshToken, setUserAccessToken])
-
-  if (loading) {
-    return <Loading />
-  }
 
   return (
     <div className="h-full">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,35 @@
 
 
-import LandingComponent from './components/LandingComponent'
+import { jwtVerify } from 'jose'
+
 import {cookies} from 'next/headers'
+import { redirect } from 'next/navigation'
+import LoginSection from '@/app/components/LoginSection'
+
+const JWT_SECRET = new TextEncoder().encode(
+  process.env.JWT_SECRET
+)
+
 
 export default async function LandingPage() {
     const cookie = await cookies()
-    const refreshToken = cookie.get('refreshToken')
+    const refreshToken = cookie.get('refreshToken')?.value
+
+    let isValidRefresh = false
+
+    if(!!refreshToken){
+      try {
+        await jwtVerify(refreshToken, JWT_SECRET)
+        isValidRefresh = true
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    if(isValidRefresh){
+      redirect('/home')
+    }
 
   return (
-    <LandingComponent hasRefreshToken={!!refreshToken}/>  
+    <LoginSection />  
   )
 }
